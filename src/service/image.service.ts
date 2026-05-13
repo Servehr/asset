9105ffad4f936d53ce758d0bc5fbe0d7a77e6f6b
Hammed <servertstng@gmail.com>
2026-05-13 17:31:11 +0100

    Improvement and multiple delete action

--- src/service/image.service.ts
+++ src/service/image.service.ts
@@ -7,15 +7,17 @@ class ImageService
 
     public async upload(base64Data: string): Promise<any>
     {
+       const company: string = 'storage/technicianswork'
+       const filePath: string = ''
        try 
        {       
-         const { ASSET_PATH } = process.env;   
+         const { ORIGIN, URL_PATH } = process.env;   
          const base64Image = base64Data.replace(/^data:image\/\w+;base64,/, "");
          const imageBuffer = Buffer.from(base64Image, 'base64')
          const fileName = `technicians-work-image_${Date.now()}.jpg`
          const uploadPath = path.join('./src/', 'passport', fileName)
          fs.writeFileSync(uploadPath, imageBuffer);
-         return  `${ASSET_PATH}${fileName}`; 
+         return  `${ORIGIN}${URL_PATH}/${fileName}`; 
        } catch (error: any) {
           throw new Error(error);
        }
@@ -23,18 +25,20 @@ class ImageService
 
     public async uploads(uploads: string): Promise<any>
     {
+       const company: string = 'storage/technicianswork'
+       const filePath: string = ''
        try 
        {       
-         const { ASSET_PATH } = process.env; 
+         const { ORIGIN, URL_PATH } = process.env; 
          let images: string[] = []
          for (let index = 0; index < uploads.length; index++) 
          {  
             const base64Image = uploads[index].replace(/^data:image\/\w+;base64,/, "");
             const imageBuffer = Buffer.from(base64Image, 'base64')
             const fileName = `technicians-work-image_${Date.now()}.jpg`
-            const uploadPath = path.join('./src/', 'testing', fileName)
+            const uploadPath = path.join('./src/', company, fileName)
             fs.writeFileSync(uploadPath, imageBuffer);
-            images.push(`${ASSET_PATH}${fileName}`)
+            images.push(`${ORIGIN}${URL_PATH}/${fileName}`)
          }         
          return  images; 
        } catch (error: any) {
@@ -45,6 +49,13 @@ class ImageService
     public async download(image: string)
     {
        return {}
+    }
+
+    public async retrieve(filename: string)
+    {
+       const company: string = 'testing'
+
+
     }
 
 
